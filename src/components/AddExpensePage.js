41adbf9fef1611beb6c19809975a5c0c5d038ab4
Ram <ramlevy@gmail.com>
2018-07-27 17:32:04 +0300

    firebase with actions

--- src/components/AddExpensePage.js
+++ src/components/AddExpensePage.js
@@ -1,19 +1,27 @@
 import React from 'react';
-import ExpenseForm from './ExpenseForm';
 import { connect } from 'react-redux';
-import { addExpense } from '../actions/expenses';
+import ExpenseForm from './ExpenseForm';
+import { startAddExpense } from '../actions/expenses';
 
+export class AddExpensePage extends React.Component {
+  onSubmit = (expense) => {
+    this.props.startAddExpense(expense);
+    this.props.history.push('/'); // Route to '/' 
+  };
+  render() {
+    return (
+      <div>
+        <h1>Add Expense</h1>
+        <ExpenseForm
+          onSubmit={this.onSubmit}
+        />
+      </div>
+    );
+  }
+}
 
-const AddExpensePage = (props) => (
-  <div>
-    <h1>Add Expense</h1>
-    <ExpenseForm 
-      onSubmit={(expense) => {
-        props.dispatch(addExpense(expense));
-        props.history.push('/'); // Route to '/' 
-      }}
-    />
-  </div>
-);
+const mapDispatchToProps = (dispatch) => ({
+  startAddExpense: (expense) => dispatch(startAddExpense(expense))
+});
 
-export default connect()(AddExpensePage);
+export default connect(undefined, mapDispatchToProps)(AddExpensePage);
--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -14,26 +14,71 @@ firebase.initializeApp(config);
 
 const database = firebase.database();
 
+export { firebase, database as default }
 
-database.ref('expenses').push({
-  description: 'Moses',
-  amount: 45,
-  createdAt: 100
-})
 
-database.ref('expenses').push({
-  description: 'BBB',
-  amount: 52,
-  createdAt: 200
-})
 
-database.ref('expenses').push({
-  description: 'Anati',
-  amount: 20,
-  createdAt: 300
-}) 
 
-database.ref('expenses/-LIHGLo3Sbs2TdJp-oNo').remove();
+
+
+
+
+
+
+
+
+
+
+
+
+// // subscribe to changes and update the array of expenses
+// database.ref().on('value', (snapshot) => {
+//   const expenses = [];
+
+//   snapshot.forEach((childSnapshot) => {
+//     expenses.push({
+//       id: childSnapshot.key,
+//       ...childSnapshot.val()
+//     });
+//   })
+//   console.log(expenses);
+// });
+
+// // child added
+// database.ref().on('child_added', (snapshot) => {
+//   console.log(snapshot.key, snapshot.val());
+// });
+
+// // child removed
+// database.ref().on('child_removed', (snapshot) => {
+//   console.log(snapshot.key, snapshot.val());
+// });
+
+// // child changed
+// database.ref().on('child_changed', (snapshot) => {
+//   console.log(snapshot.key, snapshot.val());
+// });
+
+// // Array-like push
+// database.ref('expenses').push({
+//   description: 'Moses',
+//   amount: 45,
+//   createdAt: 100
+// })
+
+// database.ref('expenses').push({
+//   description: 'BBB',
+//   amount: 52,
+//   createdAt: 200
+// })
+
+// database.ref('expenses').push({
+//   description: 'Anati',
+//   amount: 20,
+//   createdAt: 300
+// }) 
+
+// database.ref('expenses/-LIHGLo3Sbs2TdJp-oNo').remove();
 
 // // ref(path) -> reference to the location in the DB. If no path provided, returns the root of DB
 // database.ref().set({
