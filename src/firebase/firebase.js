8df2b01f8b4a23fb220a0b9bec73a9b6413b20e9
Ram <ramlevy@gmail.com>
2018-07-27 17:59:28 +0300

    setup test\dev dbs

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -2,12 +2,12 @@ import * as firebase from 'firebase'; // grab all exports into firebase -> fireb
 
 // Initialize Firebase
 const config = {
-  apiKey: "AIzaSyC0OGIW06thSKOROAsLlUCe3y73U998npo",
-  authDomain: "expensify-523c1.firebaseapp.com",
-  databaseURL: "https://expensify-523c1.firebaseio.com",
-  projectId: "expensify-523c1",
-  storageBucket: "expensify-523c1.appspot.com",
-  messagingSenderId: "519327295396"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
 };
 
 firebase.initializeApp(config);
