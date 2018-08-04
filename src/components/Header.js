398e5aa81bb65a3f1d249630dbd39f2bab563101
Ram <ramlevy@gmail.com>
2018-08-04 12:00:23 +0300

    logout button

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { startLogout } from '../actions/auth';
 
-const Header = () => (
+export const Header = ({ startLogout }) => (
   <header>
     <h1>Expensify</h1>
     <NavLink to="/" activeClassName="is-active" exact={true}> Dashboard
     </NavLink> {/* "/" is in every link name, so we use exact property */}
     <NavLink to="/create" activeClassName="is-active"> Create Expense</NavLink>
     <NavLink to="/help" activeClassName="is-active"> Help</NavLink>
+    <button onClick={startLogout}>Logout</button>
   </header>
 );
 
-export default Header;
+const mapDispatchToProps = (dispatch) => ({
+  startLogout: () => dispatch(startLogout())
+});
+
+// connect Header to store, undefined -> because no state is being used
+export default connect(undefined, mapDispatchToProps)(Header)
