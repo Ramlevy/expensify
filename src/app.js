dc7f90d3f5fed942759a69b002a69cb74f6f3a48
Ram <ramlevy@gmail.com>
2018-08-05 22:28:30 +0300

    added routing based on login\logout

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import AppRouter from './routers/AppRouter';
+import AppRouter, { history } from './routers/AppRouter';
 import configureStore from './store/configureStore';
 import { startSetExpenses } from './actions/expenses';
 import { setTextFilter } from './actions/filters';
@@ -18,19 +18,29 @@ const jsx = (
   </Provider>
 );
 
+let hasRendered = false;
+const renderApp = () => {
+  if(!hasRendered) {
+    ReactDOM.render(jsx, document.getElementById('app'))
+    hasRendered = true;
+  }
+};
+
 
 ReactDOM.render(<p>Loading...</p>, document.getElementById('app'))
 
-store.dispatch(startSetExpenses()).then(() => {
-  ReactDOM.render(jsx, document.getElementById('app'))
-});
-
 // runs on Authenticated to unAuthenticated or the opposite
 firebase.auth().onAuthStateChanged((user) => {
-  if (user) {
-    console.log('log in');
-  } else {
-    console.log('log out');
+  if (user) { // Login
+    store.dispatch(startSetExpenses()).then(() => {
+      renderApp();
+      if(history.location.pathname === '/') {
+        history.push('/dashboard');
+      }
+    });
+  } else { // Logout
+    renderApp();
+    history.push('/');
   }
 });
 
