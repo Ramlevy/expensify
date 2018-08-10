58233d7813d2b7341e74bfeac41c15efe940b855
Ram <ramlevy@gmail.com>
2018-08-10 23:45:58 +0300

    added public route

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,10 +6,9 @@ import { startLogout } from '../actions/auth';
 export const Header = ({ startLogout }) => (
   <header>
     <h1>Expensify</h1>
-    <NavLink to="/" activeClassName="is-active" exact={true}> Dashboard
+    <NavLink to="/dashboard" activeClassName="is-active"> Dashboard
     </NavLink> {/* "/" is in every link name, so we use exact property */}
     <NavLink to="/create" activeClassName="is-active"> Create Expense</NavLink>
-    <NavLink to="/help" activeClassName="is-active"> Help</NavLink>
     <button onClick={startLogout}>Logout</button>
   </header>
 );
