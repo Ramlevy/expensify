83fb037b6aa7c3f2c2fd0d2c481f40ff8668ac6f
Ram <ramlevy@gmail.com>
2018-08-12 00:11:24 +0300

    routing

---
+++ src/components/LoginPage.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { startLogin } from '../actions/auth';
+import { connect } from 'react-redux';
+
+
+export const LoginPage = ({ startLogin }) => {
+  return (
+    <div>
+      <button onClick={startLogin}>Login</button>
+    </div>
+  );
+};
+
+const mapDispatchToProps = (dispatch) => ({
+  // startLogin prop dispatch startLogin action
+  startLogin: () => dispatch(startLogin())
+});
+
+export default connect(undefined, mapDispatchToProps)(LoginPage);
+
---
+++ src/routers/PrivateRoute.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { Route, Redirect } from 'react-router-dom';
+import Header from '../components/Header';
+
+
+export const PrivateRoute = ( // ...rest = All other props that we didn't destructure specifically
+  { isAuthenticated, component: Component, ...rest }) => ( // component renamed to Component  (prop passed in AppRouter.js)
+    <Route {...rest} component={(props) => (
+      isAuthenticated ? (
+        <div>
+          <Component {...props} />
+        </div>
+      ) : (
+          <Redirect to="/" />
+        )
+    )} />
+  );
+
+const mapStateToProps = (state) => ({
+  isAuthenticated: !!state.auth.uid
+});
+
+export default connect(mapStateToProps)(PrivateRoute);
