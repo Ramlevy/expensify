e6d3baf74bae1d53bfd354c12197b54e382f2d02
Ram <ramlevy@gmail.com>
2018-08-18 15:10:29 +0300

    header css

--- src/components/ExpensesSummary.js
+++ src/components/ExpensesSummary.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { connect } from 'react-redux'; 
+import { connect } from 'react-redux';
 import numeral from 'numeral';
 import selectExpenses from '../selectors/expenses';
 import selectExpensesTotal from '../selectors/expenses-total';
@@ -8,8 +8,10 @@ const ExpensesSummary = ({ expenseCount, expensesTotal }) => {
   const expenseWord = expenseCount === 1 ? 'expense' : 'expenses';
   const formattedExpensesTotal = numeral(expensesTotal / 100).format('$0,0.00')
   return (
-    <div>
-      <h1>Viewing {expenseCount} {expenseWord} totaling {formattedExpensesTotal}</h1>
+    <div className="page-header">
+      <div className="content-container">
+        <h1 className="page-header__title">Viewing {expenseCount} {expenseWord} totaling {formattedExpensesTotal}</h1>
+      </div>
     </div>
   );
 };
