bfaab3f279784b23c25190d16d3af147f9854b6b
Ram <ramlevy@gmail.com>
2018-09-12 23:57:44 +0300

    css items list

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -8,9 +8,9 @@ const ExpenseListItem = ({ description, amount, createdAt, id }) => {
     <Link className="list-item" to={`/edit/${id}`}>
       <div>
         <h3 className="list-item__title">{description}</h3>
-        <span className="list-item__sub-title">{numeral(amount / 100).format('$0,0.00')}</span>
+        <span className="list-item__sub-title">{moment(createdAt).format('MMMM Do, YYYY')}</span>
       </div>
-      <h3 className="list-item__data">{moment(createdAt).format('MMMM Do, YYYY')}</h3>
+      <h3 className="list-item__data">{numeral(amount / 100).format('$0,0.00')}</h3>
     </Link>
   );
 };
